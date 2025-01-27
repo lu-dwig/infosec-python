1a3c5f27ecd0841620496fc7d3d704155f4491f9
lu-dwig <murimiludwig@gmail.com>
2025-01-27 20:35:25 +0300

    creation of the tcp server socket

--- server.py
+++ server.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 
+import socket
+
+#Creating the socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname() #Host is the server IP
+port = 444 #Port to listen on 
+
+    
