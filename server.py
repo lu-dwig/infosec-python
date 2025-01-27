a097848376fd2f415dc3e7899411b76ce39394f2
lu-dwig <murimiludwig@gmail.com>
2025-01-27 20:38:27 +0300

    Starting of the connection and the connection success message

--- server.py
+++ server.py
@@ -14,3 +14,15 @@ serversocket.bind((host, port)) #Host will be replaced/substitued with IP, if ch
 #Starting TCP listener
 serversocket.listen(3)
 
+while True:
+    #Starting the connection 
+    clientsocket,address = serversocket.accept()
+
+    print("received connection from " % str(address))
+    
+    #Message sent to client after successful connection
+    message = 'hello! Thank you for connecting to the server' + "\r\n"
+    
+    clientsocket.send(message)
+
+    clientsocket.close()   
