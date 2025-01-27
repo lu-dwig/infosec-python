53528c7690750cbfe6db16ab48022a54155a8bee
lu-dwig <murimiludwig@gmail.com>
2025-01-27 20:36:27 +0300

    binding of the socket and starting of the listener of the tcp server socket

--- server.py
+++ server.py
@@ -8,4 +8,9 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname() #Host is the server IP
 port = 444 #Port to listen on 
 
-    
+#Binding to socket
+serversocket.bind((host, port)) #Host will be replaced/substitued with IP, if changed and not running on host
+
+#Starting TCP listener
+serversocket.listen(3)
+
